08cea899b85eb1441645b190a86d01ff851cbb7e
Aaryan Paudel <aaryan.cs01@gmail.com>
2023-09-24 22:04:53 +0545

    feat: api hit and use effect'

--- src/components/lifecycle.component.tsx
+++ src/components/lifecycle.component.tsx
@@ -1,13 +1,33 @@
 import React, { useEffect, useState } from "react";
+import Card from "./card";
 
 function Lifecycle() {
   const [name, setName] = useState("");
+  const [data, setData] = useState(null);
+
+  //Promise asnc , await
+  // reject , resolve
+  // callback hell
+
+  const getData = async () => {
+    //get method
+    const apiData = await fetch("https://fakestoreapi.com/products");
+    const jsonData = await apiData.json();
+
+    setData(jsonData);
+    console.log(jsonData);
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
 
   return (
-    <>
-      <div>{name}</div>
-      <input onChange={(e) => setName(e.target.value)} />
-    </>
+    <div className="wrapper">
+      {data?.map((item) => (
+        <Card key={item.id} data={item} />
+      ))}
+    </div>
   );
 }
 
